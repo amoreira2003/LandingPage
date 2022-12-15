ce230607b52772713cabd400b234e234f47645c1
Alexandre Moreira <alexrmoreira777@gmail.com>
2022-12-15 16:58:23 -0300

    Implemented Form Validation With JQuery

--- index.js
+++ index.js
@@ -15,4 +15,42 @@ $(document).ready(() => {
     $(".navbar button").click(() => {
         $(".contact-form").toggle()
     })
+
+    $("#submitcontact").hide();
+
+    function validateName() {
+    let nameInputValue =  $("#nameinput").val();
+
+    return nameInputValue.length > 0 && nameInputValue !== "" ? true : false
+
+    }
+
+    function validateEmail() {
+        let emailInputValue =  $("#emailinput").val();
+        return emailInputValue.length > 0 && emailInputValue !== ""  && emailInputValue.includes(".")  && emailInputValue.includes("@")? true : false
+    }
+
+    function validateTel() {
+        let celInputValue =  $("#celinput").val();
+
+
+        return (celInputValue.length <= 11 ? true : false)
+    }
+
+
+    $("input").keyup(function (e) { 
+        if(validateEmail() && validateTel() && validateName()) {
+            $("#submitcontact").show();
+        } else {
+            $("#submitcontact").hide();
+        }
+    });
+
+
+
+
+
+
+
+
 })
